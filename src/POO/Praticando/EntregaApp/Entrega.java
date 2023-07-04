0a8666513d58edc7eea894917d530f3a0ad56bc9
arthurdiasdeveloper <arthurdiascorporativo@gmail.com>
2023-07-04 15:45:08 -0300

    update scripts + validation

--- src/POO/Praticando/EntregaApp/Entrega.java
+++ src/POO/Praticando/EntregaApp/Entrega.java
@@ -3,6 +3,7 @@ package POO.Praticando.EntregaApp;
 import POO.Praticando.EntregaModel.Cliente;
 import POO.Praticando.EntregaModel.Endereco;
 
+import java.sql.SQLSyntaxErrorException;
 import java.util.ArrayList;
 
 public class Entrega {
@@ -11,6 +12,8 @@ public class Entrega {
 
         Endereco endereco = new Endereco();
         //dados do enderecos
+        //caso eu retire o cep, terei uma exception
+        endereco.cep = "000000";
 
 
         Cliente cliente = new Cliente();
@@ -20,8 +23,16 @@ public class Entrega {
          //   Cliente.enderecos = new ArrayList<Endereco>();
         //}
 
-        cliente.getEnderecos().add(endereco);
-        System.out.println("Endreco addiconaod a cliente com sucesso!");
+        //o Try e cacth forca a exception.
+        try{
+            //cliente.getEnderecos().add(endereco);
+            //ao invés de utilizar o código acima, agora eu ustilizo o metodo que criei.
+            cliente.adicionaEndereco(endereco);
+            System.out.println("Endreco addiconaod a cliente com sucesso!");
+        }catch (Exception e){
+            System.err.println("Houve um erro ao inserir o Endereco: " + e.getMessage());
+        }
+
 
         //cliente.enderecos.add(endereco);
         //System.out.println("Foi adicionado com sucesso!");
--- src/POO/Praticando/EntregaModel/Cliente.java
+++ src/POO/Praticando/EntregaModel/Cliente.java
@@ -16,9 +16,24 @@ public class Cliente {
     //  }
     // nao deixarei o metodo set porque queremos adicinar manualmente os enderecos.
 
-    public static List<Endereco> getEnderecos(){
+    public void adicionaEndereco(Endereco endereco){
+        if(endereco == null){
+            throw new NullPointerException("O Enderecço não pode ser nulo!");
+        }
+
+        if(endereco.cep == null){
+            throw new NullPointerException("O cep náo pode ser nulo!");
+        }
+        //se eu passar por todas as validacoes
+        getEnderecos().add(endereco);
+    }
+    //agora irei colocar o metodo get como privado para obrigar
+    //a utilização do metodo adiconaEndereco
+    private static List<Endereco> getEnderecos(){
         if(enderecos == null){
            enderecos = new ArrayList<Endereco>();
+           //aqui estamos verificando se o atributo endereção não é nulo
+            // se ele não for nulo ele adiciona um endereço
         }
         return enderecos;
     }
